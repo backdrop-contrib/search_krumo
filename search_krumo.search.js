ffc7f855ddf753db5844c97b239e77e74e947fc4
ronaldtebrake <ronaldtebrake@2314038.no-reply.drupal.org>
2015-04-17 13:46:29 +0200

    Issue #2457809 by ronaldtebrake: Support for accented characters

--- search_krumo.search.js
+++ search_krumo.search.js
@@ -15,10 +15,10 @@ Drupal.behaviors.search_krumo_search = {
     if ($(k).addClass('processed').length > 0) {
       var form  = '<div class="search-krumo">';
       form     += '  <form id="search-krumo">';
-      form     += '    <input type="text" name="search-query" />';
+      form     += '    <input class="form-text" type="text" name="search-query" />';
       // If there are more than one krumo's.
       if ($(k).length > 1) {
-        form   += '    <select name="search-option">';
+        form   += '    <select class="form-select" name="search-option">';
         form   += '      <option value="all">search all</option>';
         // For each krumo.
         $(k).each(function(i) {
@@ -27,7 +27,7 @@ Drupal.behaviors.search_krumo_search = {
         });
         form   += '    </select>';
       }
-      form     += '    <input type="submit" value="submit" name="submit" />';
+      form     += '    <input class="form-submit" type="submit" value="submit" name="submit" />';
       form     += '  </form>';
       form     += '</div>';
       form     += '<div class="search-krumo-results"></div>';
@@ -56,15 +56,24 @@ Drupal.behaviors.search_krumo_search = {
           k = $('.krumo-root');
         }
         // Find all elements with the query.
-        $('.krumo-element > a:contains('+ q +'), .krumo-element > strong:contains('+ q +')', k).each(function(i) {
-          // Add result class.
-          $(this).parent().addClass('krumo-query-result');
+        $('.krumo-element > a:contains('+ q +'), .krumo-element > strong:contains('+ q +'), .krumo-preview:contains('+ q +')', k).each(function(i) {
+          if (!$(this).hasClass('krumo-preview')) {
+            // Add result class.
+            $(this).parent().addClass('krumo-query-result');
 
-          // Expand parents until the query result is layed open before the user.
-          $(this).parent().parents(".krumo-nest").show().prev().addClass('krumo-opened');
+            // Expand parents until the query result is layed open before the user.
+            $(this).parent().parents(".krumo-nest").show().prev().addClass('krumo-opened');
+          }
+          else  {
+            // We have a result in the preview (body/summary).
+            $(this).closest(".krumo-nest").prev().addClass('krumo-query-result');
+
+            // Expand parents until the query result is layed open before the user.
+            $(this).parent().parents(".krumo-nest").show().prev().addClass('krumo-opened');
+          }
         });
         // Show result overview.
-        $('.search-krumo-results').html('Found '+ $('.krumo-element > a:contains('+ q +'), .krumo-element > strong:contains('+ q +')', k).length +' elements');
+        $('.search-krumo-results').html('Found '+ $('.krumo-element > a:contains('+ q +'), .krumo-element > strong:contains('+ q +'), .krumo-preview:contains('+ q +')', k).length +' elements');
       }
       else {
         $('.search-krumo-results').html('Empty query');
